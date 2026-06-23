3fd69b2e7682bf7973765df5f50c9b59c6b11904
kdayivon <kayivon001@gmail.com>
2026-06-23 20:14:56 +0200

    feat: implementation of neural network using finite difference

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,39 @@ void main() {
         1, 1, 0,
     };
 
+    // Random generator = new Random(69);
+    // float w1 = generator.nextFloat();
+    // float w2 = generator.nextFloat();
+    // float b = generator.nextFloat();
+
+    float eps = (float) 1e-1;
+    float rate = (float) 1e-1;
+
     int n = td.length/3;
     int stride = 3; 
     int offset = 2;
     Mat ti = new Mat(n, 2, stride, 0, td);
     Mat to = new Mat(n, 1, stride, offset, td);
-    IO.print(ti);
-    IO.print(to);
 
     int[] arch = {2, 2, 1};
     NN nn = new NN(arch);
+    NN gr = new NN(arch);
     nn_rand(nn, 0, 1);
-    IO.print(nn);
+
+    System.out.printf("cost = %f\n", nn_cost(nn, ti, to));
+    for (int i = 0; i < 20*1000; i++) {
+        nn_finite_diff(nn, gr, eps, ti, to);
+        nn_learn(nn, gr, rate);
+        System.out.printf("cost = %f\n", nn_cost(nn, ti, to));
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            nn.NN_INPUT().MAT_AT(0, 0, i);
+            nn.NN_INPUT().MAT_AT(0, 1, j);
+            nn_forward(nn);
+            System.out.printf("%d ^ %d = %f\n", i, j, nn.NN_OUTPUT().MAT_AT(0, 0));
+        }
+    }
 }
+
--- src/header/nn.java
+++ src/header/nn.java
@@ -25,7 +25,7 @@ public class nn {
         }
 
         public Mat(int rows, int cols, int stride, int offset, float[] es) {
-            assert es.length == rows * cols : "array size must match rows * cols";
+            if (offset + (rows - 1) * stride + cols > es.length) throw new IllegalArgumentException("array size must match rows * cols");
             this.rows = rows;
             this.cols = cols;
             this.stride = stride;
@@ -33,12 +33,12 @@ public class nn {
             this.es = es;
         }
         
-        public float MAT_AT(int row, int col, int offset) {
-            return es[offset + (row) * stride + (col)];
+        public float MAT_AT(int row, int col) {
+            return es[(this.offset) + (row) * stride + (col)];
         }        
 
-        public void MAT_AT(int row, int col, int offset, float value) {
-            es[offset + (row) * stride + (col)] = value;
+        public void MAT_AT(int row, int col, float value) {
+            es[(this.offset) + (row) * stride + (col)] = value;
         }
 
         @Override
@@ -47,7 +47,7 @@ public class nn {
             for (int i = 0; i < this.rows; i++) {
                 sb.append("     ");
                 for (int j = 0; j < this.cols; j++) {
-                    sb.append(this.MAT_AT(i, j, this.offset)).append(" ");
+                    sb.append(this.MAT_AT(i, j)).append(" ");
                 }
                 sb.append("\n");
             }
@@ -56,11 +56,7 @@ public class nn {
     }
 
     public static Mat mat_row(Mat m, int row) {
-        float[] l = new float[m.cols];
-        for (int i = 0; i < m.cols; i++) {
-            l[i] = m.MAT_AT(row, i, 0);
-        }
-        return new Mat(1, m.cols, m.stride, m.stride, l);
+        return new Mat(1, m.cols, m.stride, m.offset + row * m.stride , m.es);
     }
 
     public static void mat_copy(Mat dst, Mat src) {
@@ -68,8 +64,8 @@ public class nn {
         if (src.rows != dst.rows) throw new IllegalArgumentException("Mats must have same number of rows");
         for (int i = 0; i < dst.rows; i++) {
             for (int j = 0; j < dst.cols; j++) {
-                float x = src.MAT_AT(i, j, 0);
-                dst.MAT_AT(i, j, 0, x);
+                float x = src.MAT_AT(i, j);
+                dst.MAT_AT(i, j, x);
             }
         }
     }
@@ -77,7 +73,7 @@ public class nn {
     public static void mat_fill(Mat m, float x) {
         for (int i = 0; i < m.rows; i++) {
             for (int j = 0; j < m.cols; j++) {
-                m.MAT_AT(i, j, 0, x);
+                m.MAT_AT(i, j,x);
             }
         }
     }
@@ -90,11 +86,10 @@ public class nn {
         for (int i = 0; i < dst.rows; i++) {
             for (int j = 0; j < dst.cols; j++) {
                 float x = 0;
-                dst.MAT_AT(i, j, 0, x);
                 for (int k = 0; k < n; k++) { // iterating over the inner size i.k * k.j
-                    x += a.MAT_AT(i, k, 0) * b.MAT_AT(k, j, 0);
-                    dst.MAT_AT(i, j, 0, x);
+                    x += a.MAT_AT(i, k) * b.MAT_AT(k, j);
                 }
+                dst.MAT_AT(i, j, x);
             }
         }
     }
@@ -104,8 +99,8 @@ public class nn {
         if (a.rows != dst.rows) throw new IllegalArgumentException("Mats must have same number of rows");
         for (int i = 0; i < dst.rows; i++) {
             for (int j = 0; j < dst.cols; j++) {
-                float x = dst.MAT_AT(i, j, 0) + a.MAT_AT(i, j, 0);
-                dst.MAT_AT(i, j, 0, x);
+                float x = dst.MAT_AT(i, j) + a.MAT_AT(i, j);
+                dst.MAT_AT(i, j, x);
             }
         }
     }
@@ -114,7 +109,7 @@ public class nn {
         for (int i = 0; i < m.rows; i++) {
             for (int j = 0; j < m.cols; j++) {
                 float x = rand_float() * (high - low) + low;
-                m.MAT_AT(i, j, 0, x);
+                m.MAT_AT(i, j, x);
             }
         }
     }
@@ -122,8 +117,8 @@ public class nn {
     public static void mat_sig(Mat m) {
         for (int i = 0; i < m.rows; i++) {
             for (int j = 0; j < m.cols; j++) {
-                float x = m.MAT_AT(i, j, 0);
-                m.MAT_AT(i, j, 0, sigmoidf(x));
+                float x = m.MAT_AT(i, j);
+                m.MAT_AT(i, j, sigmoidf(x));
             }
         }
     }
@@ -137,7 +132,7 @@ public class nn {
     // float[] arch = {input_layer = 2, hidden = 2, output = 1}
     // NN nn = new NN(arch, len(arch))
         public NN(int[] arch) {
-            assert arch.length > 0: "architecture size must be bigger than 0";
+            if( arch.length <= 0) throw new IllegalArgumentException("architecture size must be bigger than 0");
             this.count = (int) arch.length - 1;
 
            // count = len(architecture) w/o activation layer 
@@ -146,12 +141,20 @@ public class nn {
             this.bs = new Mat[count];
             this.as = new Mat[count + 1];
             
-            this.as[0] = new Mat(1, arch[0], 0);  // input layer
+            this.as[0] = new Mat(1, arch[0], arch[0]);  // input layer
             for (int i = 1; i < (count+1); i++) {
-                this.ws[i-1] = new Mat(arch[i-1], arch[i], 0);
-                this.bs[i-1] = new Mat(1, arch[i], 0);
-                this.as[i]   = new Mat(1, arch[i], 0);
+                this.ws[i-1] = new Mat(arch[i-1], arch[i], arch[i]);
+                this.bs[i-1] = new Mat(1, arch[i], arch[i]);
+                this.as[i]   = new Mat(1, arch[i], arch[i]);
             }
+        }
+
+        public Mat NN_INPUT() {
+            return as[0];
+        }
+
+        public Mat NN_OUTPUT() {
+            return as[count];
         }
 
         @Override
@@ -176,9 +179,86 @@ public class nn {
 
     public static void nn_forward(NN nn) {
         for (int i = 0; i < nn.count; i++) {
-            mat_dot(nn.as[i+1], nn.as[i], nn.ws[i]);
-            mat_sum(nn.as[i+1], nn.bs[i]);
-            mat_sig(nn.as[i+1]);
+            mat_dot(nn.as[i+1], nn.as[i], nn.ws[i]); // into the next activation layer, multiply current activation by weight
+            mat_sum(nn.as[i+1], nn.bs[i]);           // add the current biases
+            mat_sig(nn.as[i+1]);                     // activating the next layer (activation layers is nn.count+1) 
         } 
     }
+
+    public static float nn_cost(NN nn, Mat ti, Mat to) {
+        if(ti.rows != to.rows) throw new IllegalArgumentException("training data input and output rows must be the same size");
+        if(to.cols != nn.NN_OUTPUT().cols) throw new IllegalArgumentException("expected ouput cols and output cols must be the same size");
+        int n = ti.rows;
+
+        float cost = 0;
+        for (int i = 0; i < n; i++) {
+            Mat x = mat_row(ti, i);     // expected input
+            Mat y = mat_row(to, i);     // expected output
+
+            mat_copy(nn.NN_INPUT(), x);
+            nn_forward(nn);
+
+            int q = to.cols;
+            for (int j = 0; j < q; j++) {
+                float d = nn.NN_OUTPUT().MAT_AT(0, j) - y.MAT_AT(0, j);
+                cost += d*d;
+            }
+        }
+        return cost/n;
+    }
+
+    public static void nn_finite_diff(NN nn, NN gr, float eps, Mat ti, Mat to) {
+        float saved;
+        float cost = nn_cost(nn, ti, to);
+        for (int i = 0; i < nn.count; i++) {
+            // weights
+            for (int j = 0; j < nn.ws[i].rows; j++) {
+                for (int k = 0; k < nn.ws[i].cols; k++) {
+                    saved = nn.ws[i].MAT_AT(j, k);
+                    float tmp = nn.ws[i].MAT_AT(j, k) + eps; 
+                    nn.ws[i].MAT_AT(j, k, tmp);
+
+                    tmp = (nn_cost(nn, ti, to) - cost) / eps;  
+                    gr.ws[i].MAT_AT(j, k, tmp);
+                    nn.ws[i].MAT_AT(j, k, saved);
+                }
+            }
+            // biases 
+            for (int j = 0; j < nn.bs[i].rows; j++) {
+                for (int k = 0; k < nn.bs[i].cols; k++) {
+                    saved = nn.bs[i].MAT_AT(j, k);
+                    float tmp = nn.bs[i].MAT_AT(j, k) + eps; 
+                    nn.bs[i].MAT_AT(j, k, tmp);
+
+                    tmp = (nn_cost(nn, ti, to) - cost) / eps;  
+                    gr.bs[i].MAT_AT(j, k, tmp);
+                    nn.bs[i].MAT_AT(j, k, saved);
+                }
+            }
+        }
+    }
+
+    public static void nn_learn(NN nn, NN gr, float rate) {
+        for (int i = 0; i < nn.count; i++) {
+            // weights
+            for (int j = 0; j < nn.ws[i].rows; j++) {
+                for (int k = 0; k < nn.ws[i].cols; k++) {
+                    float w = nn.ws[i].MAT_AT(j, k);  // learning rate * gradient
+                    float g = gr.ws[i].MAT_AT(j, k);  // learning rate * gradient
+                    float tmp = w - rate * g;
+                    nn.ws[i].MAT_AT(j, k, tmp);
+                }
+            }
+            // biases 
+            for (int j = 0; j < nn.bs[i].rows; j++) {
+                for (int k = 0; k < nn.bs[i].cols; k++) {
+                    float w = nn.bs[i].MAT_AT(j, k); 
+                    float g = gr.bs[i].MAT_AT(j, k); 
+                    float tmp = w - rate * g;
+                    nn.bs[i].MAT_AT(j, k, tmp);
+                }
+            }
+        }
+    }
+
 }
